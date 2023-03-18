42d4fb0a3cc43140109d17887bd5b201a55221cf
tw <viraree@sina.com>
2023-03-18 18:59:57 +0800

    Lookup Table

--- src/main/java/com/fullstack/romanNumerals/FullStackApplication.java
+++ src/main/java/com/fullstack/romanNumerals/FullStackApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeApplication {
+public class FullStackApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ChallengeApplication.class, args);
+		SpringApplication.run(FullStackApplication.class, args);
 	}
 
 }
